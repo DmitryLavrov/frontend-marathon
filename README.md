f280cf56833ee163d9e3b4114ea8a855fdebfa93
DmitryLavrov <d3236562@mail.ru>
2021-06-17 22:58:16 +0300

    Lost files added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #Marathon from Vladilen Minin
 
-# Day 1 14.06.2021
+# Day 1 (14.06.2021)
 Instead of VSCode plugin "Live server" I use [browser-sync](https://browsersync.io/)
 ```shell
 # install
@@ -13,4 +13,14 @@ browser-sync start --server --files index.html
 - [jsfiddle.net](https://jsfiddle.net/DmitryLavrov/2ht8sbod/6/) - sandbox for code sources
 
 ### My result
-[https://jsfiddle.net/DmitryLavrov/2ht8sbod/6/](https://jsfiddle.net/DmitryLavrov/2ht8sbod/6/)
+[https://jsfiddle.net/DmitryLavrov/2ht8sbod/7/](https://jsfiddle.net/DmitryLavrov/2ht8sbod/7/)
+
+# Day 2 (15.06.2021)
+
+### My result
+[https://jsfiddle.net/DmitryLavrov/ts284yv6/2/](https://jsfiddle.net/DmitryLavrov/ts284yv6/2/)
+
+# Day 3 (16.06.2021)
+
+### My result
+[https://jsfiddle.net/DmitryLavrov/9qvnypLu/1/](https://jsfiddle.net/DmitryLavrov/9qvnypLu/1/)
---
+++ day-4/app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = [
+  '#77E', '#7E7', '#E77', '#7EE', '#EE7', '#E7E',
+  '#AAE', '#AEA', '#EAA', '#AEE', '#EEA', '#EAE',
+]
+
+const SQUARES_NUMBER = 500
+
+for (let i=0; i< SQUARES_NUMBER; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+  board.append(square)
+
+  square.addEventListener('mouseover', () => setColor(square))
+
+  square.addEventListener('mouseleave', () => offColor(square))
+
+}
+
+const setColor = (item) => {
+  const colorNumber = Math.floor(Math.random()*colors.length)
+  item.style.background = colors[colorNumber]
+  item.style.boxShadow = `0 0 2px ${colors[colorNumber]}, 0 0 10px ${colors[colorNumber]}`
+}
+
+const offColor = (item) => {
+  item.style.background = '#222'
+  item.style.boxShadow = `0 0 2px #000`
+}
